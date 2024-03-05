e21f1cfdb1cb461a21e9467ae969d490fc0998b9
NuhDemir <115398614+NuhDemir@users.noreply.github.com>
2024-03-05 16:17:51 +0300

    new

--- JavaScript-exercises/g-13-anchor()/script.js
+++
@@ -1,9 +0,0 @@
-// Bir elemente ID'sine göre kaydırma
-
-const element = document.getElementById("my-element");
-element.scrollIntoView();
-
-// Bir elemente CSS seçicisine göre kaydırma
-
-const element2 = document.querySelector(".my-element");
-element2.scrollIntoView();
