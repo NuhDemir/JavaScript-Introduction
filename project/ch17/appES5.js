a3d853ad8b58a216b16bec71da425836be3e6fe9
NuhDemir <drmaa217@gmail.com>
2024-12-10 16:56:11 +0300

    new

--- project/ch17/appES5.js
+++ project/ch17/appES5.js
@@ -4,50 +4,55 @@ function Course(title, instructor, image) {
     this.image = image;
 }
 
-
-function UI() {
-
-}
+function UI() {}
 
 UI.prototype.addCourseToList = function(course) {
     const list = document.getElementById('course-list');
-
-
+    const html = `
+       <tr>
+           <td><img src="img/${course.image}" alt="${course.title}" style="width: 100px;"></td>
+           <td>${course.title}</td>
+           <td>${course.instructor}</td>
+           <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+        </tr>`;
+    list.innerHTML += html;
 }
 
 UI.prototype.clearControls = function() {
-
+    document.getElementById('title').value = '';
+    document.getElementById('instructor').value = '';
+    document.getElementById('image').value = '';
 }
 
-
-
-
+// deleteCourse fonksiyonunu ekleyelim
+UI.prototype.deleteCourse = function(target) {
+    if (target.classList.contains('delete')) {
+        target.parentElement.parentElement.remove();
+    }
+}
 
 document.getElementById('new-course').addEventListener('submit', function(e) {
-
     const title = document.getElementById('title').value;
-    const instructor = document.getElementById('innstructor').value;
+    const instructor = document.getElementById('instructor').value;
     const image = document.getElementById('image').value;
 
-    console.log(title, instructor, image);
-    //create course object
+    // Nesne oluştur
     const course = new Course(title, instructor, image);
 
-    //create UI
+    // UI oluştur
     const ui = new UI();
 
-    //add course to lisst
+    // Course listesine ekle
     ui.addCourseToList(course);
 
-    //clear controls
+    // Kontrolleri temizle
     ui.clearControls();
 
-    //save to database
-
-
-    //show on the uı
-
-    d
-
     e.preventDefault();
+});
+
+// Delete butonuna tıklama olayını ekle
+document.getElementById('course-list').addEventListener('click', function(e) {
+    const ui = new UI();
+    ui.deleteCourse(e.target);
 });
